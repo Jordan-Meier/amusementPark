dd29f62528757922dfc97355a296df02ecc0198f
Jordan Meier and Simon Temple <student@epicodus.com>
2016-01-13 10:57:55 -0800

    change formatting of page, alter javascript to highlight specified divs based on height that user provides

--- js/scripts.js
+++ js/scripts.js
@@ -2,9 +2,11 @@ $(document).ready(function(){
   var height = parseInt(prompt("How tall are you in cm?"));
 
   if (height >= 102) {
-    $('#tallEnough').show();
-  } else {
-    $('#notTallEnough').show();
-  }
+    $("div.taller").addClass("allowed");
 
+
+  } else {
+    // $("div.allowed").removeClass();
+    $("div.smaller").addClass("allowed");
+  }
 });
